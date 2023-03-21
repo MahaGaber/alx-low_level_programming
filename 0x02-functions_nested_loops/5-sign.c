f25670e2688c263c486ee7f94f6765d80091707f
MahaGaber <gabermaha2@gmail.com>
2023-03-21 14:22:58 -0700

    update

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * print_sign - it takes one integer argument
+ * @n: checks integer number sign of a number
+ *
+ *
+ *Return: on success 1.or 0 or -1
+ */
+int print_sign(int n)
+{
+if (n >= 0)
+{	_putchar('+');
+	return (1);
+}
+else if (n == 0)
+{	_putchar('0');
+	return (0);
+}
+else
+{	_putchar('-');
+	return (-1);
+}
+
+}
