721f477ac901d2fdf93402a9ea84a8cd25b588c8
MahaGaber <gabermaha2@gmail.com>
2023-03-21 15:24:05 -0700

    gh

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,18 +8,13 @@
  */
 int _abs(int n)
 {
-if (n > 0)
-{
-	return (n);
-}
-else if (n == 0)
-{
-	return (0);
-}
-else if (n < 0)
-{
-	n = (-1) * n;
-	return (n);
-}
+int dig;
+
+if (n >= 0)
+dig = n;
+else
+dig = (-1) * n;
+
+return (dig);
 
 }
