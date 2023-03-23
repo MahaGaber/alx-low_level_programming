f7e17aaf02140aad613da1dfc9012874bd6fd8b0
MahaGaber <gabermaha2@gmail.com>
2023-03-23 13:27:54 -0700

    upda

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * print_numbers - it takes two integer argument
+ *
+ *Return: print numbers 0 .. 9
+ */
+
+void print_numbers(void)
+{
+	int i;
+	
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+}
