41d5fc753a22a130054f77a971573ace16e9bb51
MahaGaber <gabermaha2@gmail.com>
2023-03-23 12:33:08 -0700

    update

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * _isupper - it takes two integer argument
+ * @c: accept integer number
+ *
+ *Return: 1 on success checks for uppercase character
+ */
+
+int _isupper(int c)
+{
+	int j;
+
+	for (j = 65; j <= 90; j++)
+	{
+		if (_putchar(j) == c)
+			return (1);
+		else
+			return (0);
+	}
+}
