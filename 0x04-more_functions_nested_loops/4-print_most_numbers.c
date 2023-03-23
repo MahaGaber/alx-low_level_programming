04e7fa86712d48bf77a6da1dd99cfe443d0c6ce7
MahaGaber <gabermaha2@gmail.com>
2023-03-23 14:03:53 -0700

    up

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * print_most_numbers - it takes two integer argument
+ *
+ *Return: print numbers 0 .. 9
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 || i != 4)
+		{
+		_putchar(i + 48);
+		}
+	}
+	_putchar('\n');
+
+}
