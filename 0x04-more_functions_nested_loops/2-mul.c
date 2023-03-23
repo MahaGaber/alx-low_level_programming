860f8ae65557f831a233883eed285c2902fa62f4
MahaGaber <gabermaha2@gmail.com>
2023-03-23 13:18:58 -0700

    upd

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * mul - it takes two integer argument
+ * @a: checks integer number
+ * @b: checks integer number
+ *
+ *Return: on success absoulte value multiply to numbers
+ */
+
+int mul(int a, int b)
+{
+	int  sum;
+
+	sum = a + b;
+return (sum);
+
+}
