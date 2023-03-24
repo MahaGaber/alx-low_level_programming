4c5fd7a9f977caea0c00f0f80bbe210b7239cac9
MahaGaber <gabermaha2@gmail.com>
2023-03-23 18:56:38 -0700

    uo

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include"main.h"
+/**
+ * print_diagonal - it takes two integer argument
+ *@n : integer number
+ *Return: print line
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+if (n <= 0)
+	_putchar('\n');
+else
+{
+	for (j = 0; j <= n; j++)
+	{
+		for (i = 0; i <= j; i++)
+	{
+		_putchar(' ');
+	}
+		_putchar(92);
+		_putchar('\n');
+	}
+}
+}
+
