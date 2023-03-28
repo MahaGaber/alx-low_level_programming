4b9367ba36b7308fdaffa308c9ee1d1171060f7b
MahaGaber <gabermaha2@gmail.com>
2023-03-28 13:35:42 -0700

    hgf

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * puts_half - it takes one integer argument
+ * @str: accept pointer character
+ *
+ *Return:
+ */
+
+void puts_half(char *str)
+{
+	int n, i;
+	int counter;
+
+for (counter = 1; *str != '\0'; str++)
+	counter++;
+if (counter % 2 == 0)
+	n = counter / 2;
+else
+	n = (counter - 1) / 2;
+	for (i = n; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
