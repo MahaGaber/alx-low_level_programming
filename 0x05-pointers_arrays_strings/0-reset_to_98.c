93783deb030699fb570b6a4f65e6abd99af3e7fd
MahaGaber <gabermaha2@gmail.com>
2023-03-27 19:38:57 -0700

    up

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * reset_to_98 - it takes one integer argument
+ * @n: accept pointer integer number
+ *
+ *Return:
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
+
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * swap_int - it takes two integer argument
+ * @a: accept integer number
+ *@b:  accept integer number
+ *Return:swap between two numbers
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = *a;
+}
+
