d5dcdd9d6a53132e5de20f9e614d19921cd85756
MahaGaber <gabermaha2@gmail.com>
2023-03-30 07:54:36 -0700

    add

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * _strncat - it takes one integer argument
+ * @dest: accept pointer character
+ * @src:accept pointer character
+ * @n:number of bytes
+ *Return:pointer character
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int counter = 0, i;
+
+while (dest[counter])
+	counter++;
+
+for (i = 0; src[i] && i < n; i++)
+dest[counter + i] = src[i];
+
+dest[counter + i] = "\0";
+
+return (dest);
+}
+
