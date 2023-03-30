43d9ade6a05cdec8c03cda46efca9bbf6d702517
MahaGaber <gabermaha2@gmail.com>
2023-03-30 08:04:00 -0700

    add

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ * _strncpy - it takes one integer argument
+ * @dest: accept pointer character
+ * @src:accept pointer character
+ * @n:number of bytes
+ *Return:pointer character
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int  i;
+
+
+for (i = 0; src[i] != '\0' && i < n; i++)
+dest[i] = src[i];
+
+dest[i] = '\0';
+
+return (dest);
+}
+
