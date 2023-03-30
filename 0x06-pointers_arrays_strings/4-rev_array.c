e2ad317e2dd2b972ba0ec987f5578add8f7f9114
MahaGaber <gabermaha2@gmail.com>
2023-03-30 12:40:56 -0700

    add

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * reverse_array - it takes one integer argument
+ * @a: accept pointer character
+ * @n:accept pointer character
+ *Return:
+ */
+
+void reverse_array(int *a, int n)
+{
+int j, i, temp;
+
+for (i = 0, j = n - 1; i < n; i++, j--)
+{
+	temp = a[i];
+	a[i] = a[j];
+	a[j] = temp;
+}
+
+}
+
