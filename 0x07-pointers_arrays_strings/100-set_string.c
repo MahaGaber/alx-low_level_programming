ad1865bcfefe39243d0d869a6f08b232b13f8c64
MahaGaber <gabermaha2@gmail.com>
2023-04-03 14:16:49 -0700

    add

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * set_string - it prints the chessboard
+ * @s: accept pointer character
+ * @to: accept pointer character
+ * *Return:
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
+
+
