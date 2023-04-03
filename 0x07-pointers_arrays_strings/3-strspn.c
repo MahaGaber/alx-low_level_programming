7d0a06f88d8964e2dc40b7d233e902db9ef73614
MahaGaber <gabermaha2@gmail.com>
2023-04-03 13:18:01 -0700

    add

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,11 +10,11 @@ unsigned int _strspn(char *s, char *accept)
 {
 unsigned int i, j;
 
-for (i = 0; s[i]; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
 	for (j = 0; accept[j] != s[i]; j++)
 	{
-	if (accept[i] == '\0')
+	if (accept[j] == '\0')
 	return (i);
 	}
 }
