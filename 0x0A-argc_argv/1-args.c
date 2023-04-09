6edf6b858ec50241f365ac2641ca40d083dfe46d
MahaGaber <gabermaha2@gmail.com>
2023-04-09 10:14:08 -0700

    add

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * @argc: accept integer number
+ * @argv: pointer of character
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n",  argc - 1);
+	return (0);
+}
+
