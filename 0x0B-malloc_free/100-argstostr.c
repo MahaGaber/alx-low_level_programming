fb1ebfa47130ccefb744755419cc675d203adf22
MahaGaber <gabermaha2@gmail.com>
2023-04-12 09:04:50 -0700

    add

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -28,20 +28,23 @@ if (ac == 0 || av == NULL)
 	return (NULL);
 }
 for (i = 0; i < ac; i++, nc++)
-		nc += _strlen(av[i]);
+nc += _strlen(av[i]);
 
 s = malloc(sizeof(char) * nc + 1);
 if (s == 0)
-	return (NULL);
+{
+return (NULL);
+}
 
 for (i = 0; i < ac; i++)
+{
+	for (j = 0; av[i][j] != '\0'; j++, cmpt++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++, cmpt++)
-			s[cmpt] = av[i][j];
-
-		s[cmpt] = '\n';
-		cmpt++;
+		s[cmpt] = av[i][j];
 	}
+s[cmpt] = '\n';
+cmpt++;
+}
 s[cmpt] = '\0';
 return (s);
 }
