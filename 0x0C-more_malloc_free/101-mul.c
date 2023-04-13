69704861727497e5df6bfb5b2df259dab31d8d9d
MahaGaber <gabermaha2@gmail.com>
2023-04-13 09:17:25 -0700

    add

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -77,20 +77,20 @@ int main(int argc, char *argv[])
 		if (_atoi(argv[1]) == 0 ||  _atoi(argv[2]) == 0)
 		{
 		_put("Error ");
-		_putchar('\n');	
+		_put('\n');	
 		exit(98);
 		}
 		else
 		{
 		multiply = _atoi(argv[1]) * _atoi(argv[2]);
 		_printInt(multiply);
-		_putchar('\n');
+		_put('\n');
 		}
 	}
 	else
 	{
 		_put("Error ");
-		_putchar('\n');	
+		_put('\n');	
 		exit(98);
 	}
 
