4913a14a0b84492386ccc48a10005b28bb62f861
MahaGaber <gabermaha2@gmail.com>
2023-04-14 05:27:47 -0700

    add

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,22 @@
 #include"main.h"
 /**
+ * _mem - it allocates memory with byte
+ * @str: pointer character
+ * @c: character
+ * @number: integer
+ *Return:pointer or NULL
+ */
+
+char *_mem(char *str, char c, unsigned int number)
+{
+	char *p = s;
+
+	while (number--)
+		*s++ = b;
+
+	return (p);
+}
+/**
  * _calloc - it allocates memory using malloc
  * @size: integer
  * @nmemb: integer
@@ -18,8 +35,7 @@ ptr = malloc(nmemb * size);
 if (ptr == NULL)
 	return (NULL);
 
-for (i = 0; i < nmemb; i++)
-	ptr[i] = 0;
+_mem(ptr, 0, sizeof(int) * nmemb);
 
 return (ptr);
 }
