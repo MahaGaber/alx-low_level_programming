cb174dbf9f0d7d2aadeec0e7ee136a9ca61169dc
MahaGaber <gabermaha2@gmail.com>
2023-04-14 05:17:24 -0700

    add

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,6 +9,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 void *ptr;
+int i;
 
 if (nmemb == 0 || size == 0)
 	return (NULL);
@@ -17,8 +18,8 @@ ptr = malloc(nmemb * size);
 if (ptr == NULL)
 	return (NULL);
 
-while (nmemb--)
-	*ptr++ = 0;
+for (i = 0; i < nmemb; i++)
+	ptr[i] = 0;
 
 return (ptr);
 }
