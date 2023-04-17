a2deafd75bce7ccdcf9af191aa8aa6a097514f3c
MahaGaber <gabermaha2@gmail.com>
2023-04-17 08:54:51 -0700

    add

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include"dog.h"
+/**
+ * init_dog - it fill memory with constant byte
+ * @d: accept pointer of struct
+ * @name: pointer character
+ * @age: number
+ * @owner: pointer character
+ *Return:
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+
