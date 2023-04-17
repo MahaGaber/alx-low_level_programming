2eb158705c04762a2b04f431ee22bf582722de23
MahaGaber <gabermaha2@gmail.com>
2023-04-17 13:42:22 -0700

    add

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,10 +20,17 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+
+#endif
+
+#ifndef DOG_T_H
+#define DOG_T_H
+
 typedef struct dog dog_t
 
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
+
 
