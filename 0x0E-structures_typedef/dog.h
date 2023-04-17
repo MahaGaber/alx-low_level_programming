dbf3b542dd092b4d2a0b5d27877e903a6518ed6a
MahaGaber <gabermaha2@gmail.com>
2023-04-17 08:33:44 -0700

    add

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
