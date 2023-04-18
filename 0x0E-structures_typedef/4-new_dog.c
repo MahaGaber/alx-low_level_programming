50187ca686587e2ee202947ce40741c5e208f1f1
MahaGaber <gabermaha2@gmail.com>
2023-04-18 02:15:21 -0700

    add

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -47,20 +47,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
 	
-	if (!name || !owner || age < 0)
-		return (NULL);
-		
 	d = (dog_t *)malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
 
 	d->name = malloc(sizeof(char) *  (_strlen(name) + 1));
 	if (d->name == NULL)
+	{
+		free(d);
 		return (NULL);
+	}
 
 	d->owner = malloc(sizeof(char) *  (_strlen(owner) + 1));
 	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
 		return (NULL);
+	}
 
 	d->name = _strcpy(d->name, name);
 	d->age = age;
