e942638705e3be0074f5bb765df91074d9764b86
MahaGaber <gabermaha2@gmail.com>
2023-04-21 12:58:12 -0700

    add

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include"3-calc.h"
+/**
+ * main - Entry point
+* @argc: accept integer number
+ * @argv: pointer of character
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
+	if (!op_func)
+	{
+	printf("Error\n");
+	exit(99);
+	}
+	if (b == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
+	{
+	printf("Error\n");
+	exit(100);
+	}
+	printf("%d\n", op_func(a, b));
+	return (0);
+}
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include"3-calc.h"
+
+/**
+ * op_add - it add
+ * @a: integer
+ * @b:integer
+ *Return:integer
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - it subtract
+ * @a: integer
+ * @b:integer
+ *Return:integer
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - it add
+ * @a: integer
+ * @b:integer
+ *Return:integer
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - it divide
+ * @a: integer
+ * @b:integer
+ *Return:integer
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - it add
+ * @a: integer
+ * @b:integer
+ *Return:integer
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
+
