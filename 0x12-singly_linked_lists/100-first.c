09097b14d554aeb6a4869b999e7940ca40dd5d34
MahaGaber <gabermaha2@gmail.com>
2023-04-27 19:58:11 -0700

    add

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include"lists.h"
+
+void _constructor(void) __attribute__ ((constructor));
+
+/**
+ * _constructor - it execute before main()
+ *
+ *Return:void
+ */
+
+void _constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
+
+
