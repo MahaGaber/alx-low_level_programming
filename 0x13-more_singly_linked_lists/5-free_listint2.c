5eb220fa2ddd3012d561397ef14272cc408fd403
MahaGaber <gabermaha2@gmail.com>
2023-04-29 06:26:31 -0700

    add

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include"lists.h"
+
+/**
+ * free_listint2 - it free list
+ * @head: pointer of list
+ *Return:
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *next_node;
+
+	current = *head;
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current);
+		current = next_node;
+	}
+	*head = NULL;
+}
+
+
