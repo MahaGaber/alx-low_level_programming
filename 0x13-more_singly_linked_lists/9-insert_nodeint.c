89e20c30ee5e4175fbb5663d88fd5efa9c589445
MahaGaber <gabermaha2@gmail.com>
2023-05-03 14:12:55 -0700

    add

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,23 @@
 #include"lists.h"
 
 /**
+ * calc_size - caculate size of linked list
+ * @head: pointer of list
+ *Return:integer
+*/
+unsigned int calc_size(listint_t *head)
+{
+	unsigned int size = 0;
+
+	while (head != NULL)
+	{
+		head = head->next;
+		size++;
+	}
+	return (size);
+}
+
+/**
  * insert_nodeint_at_index - inserts a new node at a given position
  * @head: pointer of list
  * @idx: integer index
@@ -12,7 +29,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node;
 	listint_t *current;
-	unsigned int i = 0;
+	unsigned int i = 0, size;
+
+	size = calc_size(*head);
+	if (idx > size || idx < 0)
+		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL || head == NULL)
