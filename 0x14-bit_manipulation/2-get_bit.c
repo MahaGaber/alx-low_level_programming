38c373ac994d999aafcb830511cb405591ea9149
MahaGaber <gabermaha2@gmail.com>
2023-05-05 05:11:14 -0700

    add

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,11 +9,7 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int n_bit;
-
-	n_bit = sizeof(n) * 8;
-
-	if (index == n_bit)
+	if (index == sizeof(n) * 8)
 		return (-1);
 
 	return (n >> index & 1);
