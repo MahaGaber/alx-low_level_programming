20b6e01893ce68ebf03659a15cf5640b709af6a8
MahaGaber <gabermaha2@gmail.com>
2023-05-08 14:32:00 -0700

    add

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	creat_f = open(filename, O_RDWR | O_CREAT | O_TRUNC, 600);
+	creat_f = open(filename, O_CREAT | O_RDWR |  O_TRUNC, 0600);
 	if (creat_f == -1)
 		return (-1);
 
