6604dcc26db6e3bb34e718a5aca49ab5dba9426c
MahaGaber <gabermaha2@gmail.com>
2023-05-10 09:21:01 -0700

    add

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,28 +12,28 @@ int main(int argc, char *argv[])
 	char buf[1024];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+		dprintf(STDERR_FILENO, USAGE), exit(97);
 	file_from = open(argv[1], O_RDONLY);
-if (file_from == -1)
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	file_to = open(argv[2], O_CREAT | O_WRONLY |  O_TRUNC, 0664);
+	if (file_from == -1)
+		dprintf(STDERR_FILENO, ERROR_READ, argv[1]), exit(98);
+	file_to = open(argv[2], O_CREAT | O_WRONLY |  O_TRUNC, PERM);
 	if (file_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, ERROR_WRITE, argv[2]), exit(99);
 	num_bytes_read = read(file_from, buf, 1024);
 	while (num_bytes_read > 0)
 	{
 		if (write(file_to, buf, num_bytes_read) != num_bytes_read)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+			dprintf(STDERR_FILENO, ERROR_WRITE, argv[2]), exit(99);
 	}
 
-if (num_bytes_read == -1)
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	if (num_bytes_read == -1)
+		dprintf(STDERR_FILENO, ERROR_READ, argv[1]), exit(98);
 	file_from = close(file_from);
 	file_to = close(file_to);
 	if (file_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+		dprintf(STDERR_FILENO, ERROR_CLOSE, file_from), exit(100);
 	if (file_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+		dprintf(STDERR_FILENO, ERROR_CLOSE, file_to), exit(100);
 
 	return (0);
 }
